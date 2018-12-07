eec6c5696c65c6afa7cb3f6bbaea00dd4ec5599e
moshuhui <343420553@qq.com>
2018-12-07 17:15:01 +0800

    add mobile html 1.0

---
+++ mobile/js/jqtab.js
@@ -0,0 +1,43 @@
+// JavaScript Document
+$(document).ready(function() {		
+	
+	$(function(){
+		$(".nav li").hover(function(){
+			$(this).find("div").slideToggle("slow");
+		},function(){
+			$(this).find("div").slideToggle("slow");
+		});
+	});
+	
+	$(".index_2_3 span").hover(function() {
+		$(this).addClass("iconavbg01"); //为被点击的选项卡添加"now_focus"类
+		$(this).siblings().removeClass("iconavbg01"); //去掉其它选项卡的"now_focus"类
+		var $dangqian = $(".index_2_4 > .index_2_4_1").eq($(".index_2_3 span").index(this)); //获取到和被点击选项卡顺序相同的内容容器
+		$dangqian.addClass("showdiv01"); //为这个内容容器添加"now_focus"类
+		$dangqian.siblings().removeClass("showdiv01"); //去掉其它内容容器的"now_focus"类
+	});
+	
+	
+	$(function(){
+		$(".index_1_2_2 li").hover(function(){
+			$(this).find("em").css("color","#2e8908");
+		},function(){
+			$(this).find("em").css("color","#DDDDDD");
+		});
+	});
+	
+	
+	$(function(){
+		$(".index_2_1 li").hover(function(){
+			$(this).find("span").css("color","#dd7206");
+			$(this).find("em").css("color","#dd7206");
+		},function(){
+			$(this).find("span").css("color","#bfbfbf");
+			$(this).find("em").css("color","#bfbfbf");
+		});
+	});
+			
+	
+});
+
+
